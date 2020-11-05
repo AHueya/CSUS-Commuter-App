87c348f3216e73a0a684d351cc4974bb31ebfe0b
Andersen Huey <35656323+AHueya@users.noreply.github.com>
2020-11-05 13:12:22 -0800

    Fixed locking on dropdown

--- ranking.js
+++ ranking.js
@@ -6,7 +6,7 @@ var btn = document.getElementById("myBtn");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
+// When the user clicks the button, open the modal
 btn.onclick = function() {
     modal.style.display = "block";
 }
@@ -31,57 +31,57 @@ var c = document.getElementById("ranks3")
   var value = a.options[a.selectedIndex].value;
   var valueb = b.options[b.selectedIndex].value;
   var valuec = c.options[c.selectedIndex].value;
-  
+
     if(value == "money") {
       document.getElementById("emptyrank1").innerHTML = "Rank 1: Money"
-        document.getElementById("ranks2").options[0].disabled = true;
-        document.getElementById("ranks3").options[0].disabled = true;
-        
+        document.getElementById("ranks2").options[1].disabled = true;
+        document.getElementById("ranks3").options[1].disabled = true;
+
     }
     else if (value == "time") {
       document.getElementById("emptyrank1").innerHTML = "Rank 1: Time"
-        document.getElementById("ranks2").options[1].disabled = true;
-        document.getElementById("ranks3").options[1].disabled = true;
-    }
-    else {
-        document.getElementById("emptyrank1").innerHTML = "Rank 1: Sustainability"
         document.getElementById("ranks2").options[2].disabled = true;
         document.getElementById("ranks3").options[2].disabled = true;
+    }
+    else if (value == "sustainability") {
+        document.getElementById("emptyrank1").innerHTML = "Rank 1: Sustainability"
+        document.getElementById("ranks2").options[3].disabled = true;
+        document.getElementById("ranks3").options[3].disabled = true;
     }
 
     if(valueb == "money") {
       document.getElementById("emptyrank2").innerHTML = "Rank 2: Money"
-        document.getElementById("ranks").options[0].disabled = true;
-        document.getElementById("ranks3").options[0].disabled = true;
-    }
-
-    else if (valueb == "time") {
-      document.getElementById("emptyrank2").innerHTML = "Rank 2: Time"
         document.getElementById("ranks").options[1].disabled = true;
         document.getElementById("ranks3").options[1].disabled = true;
     }
 
-    else {
-        document.getElementById("emptyrank2").innerHTML = "Rank 2: Sustainability"
+    else if (valueb == "time") {
+      document.getElementById("emptyrank2").innerHTML = "Rank 2: Time"
         document.getElementById("ranks").options[2].disabled = true;
         document.getElementById("ranks3").options[2].disabled = true;
     }
 
-    if(valuec == "money") {
-      document.getElementById("emptyrank3").innerHTML = "Rank 3: Money"
-        document.getElementById("ranks").options[0].disabled = true;
-        document.getElementById("ranks2").options[0].disabled = true;
+    else if (valueb == "sustainability") {
+        document.getElementById("emptyrank2").innerHTML = "Rank 2: Sustainability"
+        document.getElementById("ranks").options[3].disabled = true;
+        document.getElementById("ranks3").options[3].disabled = true;
     }
 
-    else if (valuec == "time") {
-      document.getElementById("emptyrank3").innerHTML = "Rank 3: Time"
+    if(valuec == "money") {
+      document.getElementById("emptyrank3").innerHTML = "Rank 3: Money"
         document.getElementById("ranks").options[1].disabled = true;
         document.getElementById("ranks2").options[1].disabled = true;
     }
 
-    else {
-        document.getElementById("emptyrank3").innerHTML = "Rank 3: Sustainability"
+    else if (valuec == "time") {
+      document.getElementById("emptyrank3").innerHTML = "Rank 3: Time"
         document.getElementById("ranks").options[2].disabled = true;
         document.getElementById("ranks2").options[2].disabled = true;
+    }
+
+    else if (value == "sustainability") {
+        document.getElementById("emptyrank3").innerHTML = "Rank 3: Sustainability"
+        document.getElementById("ranks").options[3].disabled = true;
+        document.getElementById("ranks2").options[3].disabled = true;
     }
 }
